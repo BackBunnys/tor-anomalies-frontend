cca2c4f72e3d41322f035084839c24bbce321b7a
BSC Soldier <solodov1994roma@gmail.com>
2025-01-29 16:25:50 +0300

    Version to present

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,74 +1,249 @@
-import { Image, StyleSheet, Platform } from 'react-native';
+import React, { useEffect, useState } from "react";
+import { readRemoteFile } from 'react-papaparse';
+import { Row, Col, Button, Modal, Form, Input, FormProps, ConfigProvider, Select, Segmented, Flex, DatePicker, theme, Layout } from 'antd'
+import TargetPlot from "@/components/ui/target-plot";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { faPlus, faEllipsisVertical } from '@fortawesome/free-solid-svg-icons'
+import ru_RU from 'antd/locale/ru_RU'
+import dayjs, { Dayjs } from "dayjs";
+import { Content, Header } from "antd/lib/layout/layout";
+import { MoonOutlined, SunOutlined } from "@ant-design/icons";
 
-import { HelloWave } from '@/components/HelloWave';
-import ParallaxScrollView from '@/components/ParallaxScrollView';
-import { ThemedText } from '@/components/ThemedText';
-import { ThemedView } from '@/components/ThemedView';
+library.add(faPlus, faEllipsisVertical)
 
-export default function HomeScreen() {
-  return (
-    <ParallaxScrollView
-      headerBackgroundColor={{ light: '#A1CEDC', dark: '#1D3D47' }}
-      headerImage={
-        <Image
-          source={require('@/assets/images/partial-react-logo.png')}
-          style={styles.reactLogo}
-        />
-      }>
-      <ThemedView style={styles.titleContainer}>
-        <ThemedText type="title">Welcome!</ThemedText>
-        <HelloWave />
-      </ThemedView>
-      <ThemedView style={styles.stepContainer}>
-        <ThemedText type="subtitle">Step 1: Try it</ThemedText>
-        <ThemedText>
-          Edit <ThemedText type="defaultSemiBold">app/(tabs)/index.tsx</ThemedText> to see changes.
-          Press{' '}
-          <ThemedText type="defaultSemiBold">
-            {Platform.select({
-              ios: 'cmd + d',
-              android: 'cmd + m',
-              web: 'F12'
-            })}
-          </ThemedText>{' '}
-          to open developer tools.
-        </ThemedText>
-      </ThemedView>
-      <ThemedView style={styles.stepContainer}>
-        <ThemedText type="subtitle">Step 2: Explore</ThemedText>
-        <ThemedText>
-          Tap the Explore tab to learn more about what's included in this starter app.
-        </ThemedText>
-      </ThemedView>
-      <ThemedView style={styles.stepContainer}>
-        <ThemedText type="subtitle">Step 3: Get a fresh start</ThemedText>
-        <ThemedText>
-          When you're ready, run{' '}
-          <ThemedText type="defaultSemiBold">npm run reset-project</ThemedText> to get a fresh{' '}
-          <ThemedText type="defaultSemiBold">app</ThemedText> directory. This will move the current{' '}
-          <ThemedText type="defaultSemiBold">app</ThemedText> to{' '}
-          <ThemedText type="defaultSemiBold">app-example</ThemedText>.
-        </ThemedText>
-      </ThemedView>
-    </ParallaxScrollView>
-  );
+class DataRow {
+  date: string;
+  value: number;
+  target: string
+
+  constructor(date: string, value: number, target: string) {
+    this.date = date;
+    this.value = value;
+    this.target = target;
+  }
 }
 
-const styles = StyleSheet.create({
-  titleContainer: {
-    flexDirection: 'row',
-    alignItems: 'center',
-    gap: 8,
-  },
-  stepContainer: {
-    gap: 8,
-    marginBottom: 8,
-  },
-  reactLogo: {
-    height: 178,
-    width: 290,
-    bottom: 0,
-    left: 0,
-    position: 'absolute',
-  },
-});
+class Target {
+  name: string;
+  countries: string[]
+  data: DataRow[];
+
+  constructor(name: string, data: DataRow[], countries: string[]) {
+    this.name = name;
+    this.data = data;
+    this.countries = countries;
+  }
+}
+
+type FieldType = {
+  targets: {value: string, label: string}[];
+  name: string;
+};
+
+enum Type {
+  RELAY,
+  BRIDGE,
+  ALL
+}
+
+type Interval = {
+  start?: Dayjs | null;
+  end?: Dayjs | null;
+}
+
+enum Theme {
+  LIGHT,
+  DARK
+}
+
+export default function HomeScreen() {
+  const [ targets, setTargets] = useState<Target[]>([])
+  const [ isModalOpen, setIsModalOpen] = useState<boolean>(false);
+  const [ manuallyChanged, setManuallyChanged ] = useState<boolean>(false);
+  const [ form ] = Form.useForm();
+  const [ type, setType ] =  useState<Type>(Type.RELAY);
+  const [ interval, setInterval] = useState<Interval>({start: dayjs().add(-1, "year"), end: dayjs()})
+  const [ userTheme, setUserTheme ] = useState<Theme>(Theme.LIGHT)
+
+  useEffect(() => {
+    Promise.all(targets.map(target => (getData(target.countries).then(data => ({...target, data})))))
+      .then(newTargets => setTargets(newTargets));
+  }, [type, interval]);
+
+  function onAddButtonClick() {
+    setIsModalOpen(true);
+  }
+
+  function onModalOk() {
+    form.submit();
+  }
+
+  function onModalCancel() {
+    setIsModalOpen(false);
+    setManuallyChanged(false);
+  }
+
+  const onFinish: FormProps<FieldType>['onFinish'] = async (values) => {
+    onModalCancel();
+    let countries = values.targets.map(target => target.value);
+    let data = await getData(countries);
+    setTargets([...targets, {name: values.name, data, countries}]);
+  };
+
+  function fillName() {
+    let targets: { value: string; label: string }[] = form.getFieldValue('targets');
+    
+    if (manuallyChanged) return;
+
+    let label = (targets ?? []).map(target => target.label).join(', ')
+    form.setFieldValue('name', label);
+  }
+
+  async function fetchData(link: string, target: string): Promise<DataRow[]>{
+    return new Promise<DataRow[]>(
+      resolve => {
+        readRemoteFile(link, {
+          complete: (results) => {
+            let data = results.data;
+            resolve(data.slice(6).map((row: any) => new DataRow(row[0] as string, Number.parseInt(row[2]), target)))
+          },
+          download: true,
+          skipEmptyLines: true
+        });
+      }
+    );
+  }
+
+  async function fetchRelay(target: string) {
+    let relayLink = `https://metrics.torproject.org/userstats-relay-country.csv?start=${interval.start?.toISOString()}&end=${interval.end?.toISOString()}&country=${target}`;
+    return fetchData(relayLink, target);
+  }
+
+  async function fetchBridge(target: string) {
+    let bridgeLink = `https://metrics.torproject.org/userstats-bridge-country.csv?start=${interval.start?.toISOString()}&end=${interval.end?.toISOString()}&country=${target}`
+    return fetchData(bridgeLink, target);
+  }
+
+  function groupData(data: DataRow[][], target: string): DataRow[] {
+    let dateCount = new Map<string, number>();
+
+    data.forEach(row => {
+      row.forEach(column => {
+        dateCount.set(column.date, (dateCount.get(column.date) ?? 0) + column.value)
+      })
+    });
+
+    return [...dateCount.entries().map(entry => ({'date': entry[0], 'value': entry[1], target}))];
+  }
+
+  async function fetch(target: string): Promise<DataRow[]> {
+    if (type == Type.RELAY) return fetchRelay(target);
+    if (type == Type.BRIDGE) return fetchBridge(target);
+    if (type == Type.ALL) return Promise.all([fetchRelay(target), fetchBridge(target)]).then(results => groupData(results, target ));
+    return Promise.resolve([])
+  }
+
+  async function getData(targets: string[]): Promise<DataRow[]> {
+    return Promise.all<DataRow[]>(
+      targets.map(target => fetch(target))
+    ).then(results => {
+      return results.flatMap(data => data);
+    });
+  }
+
+  function deleteTarget(idx: number) {
+    setTargets(targets.filter((target, i) => i != idx));
+  }
+
+  return (
+    <ConfigProvider locale={ru_RU} theme={{
+      algorithm: userTheme == Theme.DARK ? theme.darkAlgorithm : theme.defaultAlgorithm
+    }}>
+      <Layout>
+        <Header style={{height: '60pt'}}>
+          <Flex justify="space-between" align="middle">
+            <Flex justify="center" align="middle" flex={1}>
+              <Flex vertical gap={5} align="middle" style={{padding: 5}}>
+                <Flex justify="center" align="middle">                    
+                  <Segmented
+                    value={type} onChange={type => setType(type)}
+                    options={[
+                        { value: Type.RELAY, label: 'Relay' },
+                        { value: Type.BRIDGE, label: 'Bridge' },
+                        { value: Type.ALL, label: 'Все' },
+                    ]}
+                  />
+                </Flex>
+                <DatePicker.RangePicker value={[interval.start, interval.end]} onChange={values => setInterval({start: values?.[0], end: values?.[1]})}></DatePicker.RangePicker>
+              </Flex>
+            </Flex>
+            <Flex vertical justify="center">
+              <Segmented size="large"
+                value={userTheme} onChange={theme => setUserTheme(theme)} 
+                options={[
+                      { value: Theme.LIGHT, icon: <SunOutlined />, },
+                      { value: Theme.DARK, icon: <MoonOutlined /> },
+              ]}/>
+            </Flex>
+          </Flex>
+        </Header>
+        <Content style={{overflowY: 'scroll'}}>
+        <Row gutter={[16, 16]} style={{margin: 5}}>
+          {
+            targets.map((target, i) => (
+              <Col key={i} xxl={8} lg={12} md={24}>
+                {<TargetPlot stats={target.data} targetName={target.name} onDelete={() => deleteTarget(i)}></TargetPlot>}
+              </Col>
+            ))
+          }
+          <Col xxl={8} lg={12} md={24} style={{width: '100%'}}>
+            <Button color="primary" variant="dashed" style={{height: '100%', width: '100%'}} onClick={onAddButtonClick}>
+              <FontAwesomeIcon style={{width: "100%", height: "100%", maxWidth: '400px', maxHeight: '400px'}} icon={"plus"}></FontAwesomeIcon> 
+            </Button>
+          </Col>
+        </Row>
+        <Modal title="Новый объект интереса" 
+          open={isModalOpen} destroyOnClose
+          onOk={onModalOk} onCancel={onModalCancel} 
+          okButtonProps={{ autoFocus: true, htmlType: 'submit' }}
+          modalRender={(dom) => (
+            <Form
+              layout="vertical"
+              form={form}
+              name="newTargetForm"
+              initialValues={{ modifier: 'public' }}
+              clearOnDestroy
+              onFinish={onFinish}
+            >
+              {dom}
+            </Form>
+          )}
+        >
+          <Form.Item<FieldType>
+            label="Страны"
+            name="targets"
+          >
+            <Select mode="multiple" labelInValue onChange={() => fillName()} options={[
+              {label: 'Россия', value: 'ru'},
+              {label: 'США', value: 'us'},
+              {label: 'Эстония', value: 'ee'},
+              {label: 'Латвия', value: 'lv'},
+              {label: 'Германия', value: 'de'},
+              {label: 'Нидерланды', value: 'nl'},
+            ]}/>
+          </Form.Item>
+
+          <Form.Item<FieldType>
+            label="Название"
+            name="name"
+            rules={[{ required: true, message: 'Введите название!' }, {min: 3, message: 'Минимум 3 символа!'}]}
+            >
+              <Input onChange={() => setManuallyChanged(true)}/>
+          </Form.Item>
+        </Modal>
+        </Content>
+      </Layout>
+    </ConfigProvider>
+  );
+}
---
+++ components/ui/target-plot.tsx
@@ -0,0 +1,104 @@
+import { Area, Heatmap } from "@ant-design/plots";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { Button, Card, ConfigProvider, Dropdown, Flex, Modal, Segmented, theme } from "antd";
+import { AreaChartOutlined, DeleteOutlined, HeatMapOutlined } from "@ant-design/icons"
+import React, { useContext, useState } from "react";
+import moment from "moment";
+
+enum Type {
+    LINE,
+    HEAT
+}
+
+export default function TargetPlot({stats, targetName, onDelete} : any) {
+    const t = useContext(ConfigProvider.ConfigContext).theme;
+    const [isModalOpen, setIsModalOpen] = useState<boolean>(false)
+    const [type, setType] = useState<Type>(Type.LINE);
+
+    const config = {
+        data: stats ?? [],
+        padding: 'auto',
+        xField: 'date',
+        yField: 'value',
+        slider: {
+            x: {
+                values: [0.8, 1],
+            },
+        },
+        colorField: 'target',
+        animate: { enter: { type: 'growInX' } },
+        stack: true,
+        theme: t?.algorithm == theme.darkAlgorithm ? 'classicDark' : 'classic'
+    };
+
+    const getISOWeekStart = (dateString: string) => {
+        return moment(dateString).startOf("week").format("YYYY-MM-DD")
+    };
+
+    function groupData(data: any[]) {
+        let dateCount = new Map<string, number>();
+
+        data.forEach(row => {
+            dateCount.set(row.date, (dateCount.get(row.date) ?? 0) + row.value)
+        })
+
+        return [...dateCount.entries().map(entry => ({'date': entry[0], 'value': entry[1]}))];
+    }
+
+    const heatConfig = {
+        data: groupData(stats),
+        xField: (value: any) => getISOWeekStart(value.date),
+        yField: (value: any) => new Date(value.date).toLocaleString("ru", { weekday: "short" }),
+        colorField: 'value',
+        legend: {},
+        mark: 'cell',
+        tooltip: {
+          title: 'value',
+          field: 'value',
+          name: 'Кол-во пользователей'
+        },
+        animate: null,
+        slider: {
+            x: {
+                values: [0.8, 1],
+            },
+        },
+        sizeField: "target",
+        theme: t?.algorithm == theme.darkAlgorithm ? 'classicDark' : 'classic'
+      };
+
+    const menuItems = [
+        {
+            label: 'Удалить',
+            key: '1',
+            icon: <DeleteOutlined twoToneColor="red" />,
+            danger: true,
+            onClick: onDelete
+        }  
+    ]
+
+    return (
+        <>  
+            <Card title={targetName} bordered={false} hoverable extra={
+                <Dropdown menu={{ items: menuItems }} placement="bottomLeft">
+                            <Button type="text"><FontAwesomeIcon icon={"ellipsis-vertical"}/></Button>
+                        </Dropdown>
+                } onDoubleClick={() => setIsModalOpen(true)} style={{height: '100%'}}>
+                <Area {...config} />
+            </Card>
+            <Modal title={targetName} open={isModalOpen} width="100%" onClose={() => setIsModalOpen(false)} onCancel={() => setIsModalOpen(false)} destroyOnClose>
+                <Flex justify="end">
+                    <Segmented size="large"
+                        value={type} onChange={type => setType(type)}
+                        options={[
+                            { value: Type.LINE, icon: <AreaChartOutlined /> },
+                            { value: Type.HEAT, icon: <HeatMapOutlined />},
+                        ]}
+                    />
+                </Flex>
+                {type == Type.LINE && <Area {...config} />}
+                {type == Type.HEAT && <Heatmap {...heatConfig} />}
+            </Modal>
+        </>
+    );
+}
