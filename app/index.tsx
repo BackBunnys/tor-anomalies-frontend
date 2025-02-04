74cb770547cfaf793700a386beefca8a0da6353e
BSC Soldier <solodov1994roma@gmail.com>
2025-02-04 18:08:34 +0300

    Refactored to backend processing, added anomaly detection

--- app/index.tsx
+++ app/index.tsx
@@ -1,5 +1,4 @@
 import React, { useEffect, useState } from "react";
-import { readRemoteFile } from 'react-papaparse';
 import { Modal, Form, Input, FormProps, ConfigProvider, Select, Segmented, Flex, DatePicker, theme, Layout, List, FloatButton, Col, Row } from 'antd'
 import { library } from '@fortawesome/fontawesome-svg-core'
 import { faPlus, faEllipsisVertical } from '@fortawesome/free-solid-svg-icons'
@@ -11,15 +10,35 @@ import TargetPlot from "@/components/ui/target-plot";
 
 library.add(faPlus, faEllipsisVertical)
 
-class DataRow {
-  date: string;
-  value: number;
-  target: string
+type Metric = {
+  date: Date;
+  users: number;
+  country: string;
+}
 
-  constructor(date: string, value: number, target: string) {
+type AnomalyInterval = {
+  start: Date;
+  end: Date;
+}
+
+export type Anomaly = {
+  interval: AnomalyInterval;
+}
+
+type Response = {
+  metrics: Metric[];
+  anomalies: Anomaly[]; 
+}
+
+class DataRow {
+  date: Date;
+  users: number;
+  country: string
+
+  constructor(date: Date, value: number, target: string) {
     this.date = date;
-    this.value = value;
-    this.target = target;
+    this.users = value;
+    this.country = target;
   }
 }
 
@@ -27,6 +46,7 @@ class Target {
   name: string;
   countries: string[]
   data?: DataRow[];
+  anomalies?: Anomaly[];
 
   constructor(name: string, data: DataRow[], countries: string[]) {
     this.name = name;
@@ -68,7 +88,7 @@ export default function Index() {
 
   useEffect(() => {
     setTargets(targets.map(target => ({...target, data: undefined})));
-    Promise.all(targets.map(target => (getData(target.countries).then(data => ({...target, data})))))
+    Promise.all(targets.map(target => (getData(target.countries).then(data => ({...target, data: data.metrics, anomalies: data.anomalies})))))
       .then(newTargets => setTargets(newTargets));
   }, [type, interval]);
 
@@ -87,11 +107,12 @@ export default function Index() {
 
   const onFinish: FormProps<FieldType>['onFinish'] = async (values) => {
     onModalCancel();
+    console.log("ON FINISH")
     let countries = values.targets.map(target => target.value);
     let target: Target = {name: values.name, countries}
     setTargets([...targets, target]);
     let data = await getData(countries);
-    setTargets([...targets.filter(t => t != target), {name: values.name, data, countries}]);
+    setTargets([...targets.filter(t => t != target), {name: values.name, data: data.metrics, anomalies: data.anomalies, countries}]);
   };
 
   function fillName() {
@@ -103,56 +124,35 @@ export default function Index() {
     form.setFieldValue('name', label);
   }
 
-  async function fetchData(link: string, target: string): Promise<DataRow[]>{
-    return new Promise<DataRow[]>(
-      resolve => {
-        readRemoteFile(link, {
-          complete: (results) => {
-            let data = results.data;
-            resolve(data.slice(6).map((row: any) => new DataRow(row[0] as string, Number.parseInt(row[2]), target)))
-          },
-          download: true,
-          skipEmptyLines: true
-        });
-      }
-    );
+  async function fetchData(link: string): Promise<Response>{
+    return fetch(link)
+      .then(response => response.json());
   }
 
-  async function fetchRelay(target: string) {
+  async function fetchRelay(target: string[]) {
     let relayLink = `http://localhost:8080/v1/metrics/relays?dateInterval.start=${interval.start?.toISOString()}&dateInterval.end=${interval.end?.toISOString()}&countries=${target}`;
-    return fetchData(relayLink, target);
+    return fetchData(relayLink);
   }
 
-  async function fetchBridge(target: string) {
+  async function fetchBridge(target: string[]) {
     let bridgeLink = `http://localhost:8080/v1/metrics/bridges?dateInterval.start=${interval.start?.toISOString()}&dateInterval.end=${interval.end?.toISOString()}&countries=${target}`
-    return fetchData(bridgeLink, target);
+    return fetchData(bridgeLink);
   }
 
-  function groupData(data: DataRow[][], target: string): DataRow[] {
-    let dateCount = new Map<string, number>();
-
-    data.forEach(row => {
-      row.forEach(column => {
-        dateCount.set(column.date, (dateCount.get(column.date) ?? 0) + column.value)
-      })
-    });
-
-    return [...dateCount.entries().map(entry => ({'date': entry[0], 'value': entry[1], target}))];
+  async function fetchAll(target: string[]) {
+    let allLink = `http://localhost:8080/v1/metrics/all?dateInterval.start=${interval.start?.toISOString()}&dateInterval.end=${interval.end?.toISOString()}&countries=${target}`
+    return fetchData(allLink);
   }
 
-  async function fetch(target: string): Promise<DataRow[]> {
-    if (type == Type.RELAY) return fetchRelay(target);
-    if (type == Type.BRIDGE) return fetchBridge(target);
-    if (type == Type.ALL) return Promise.all([fetchRelay(target), fetchBridge(target)]).then(results => groupData(results, target ));
-    return Promise.resolve([])
+  async function fetchTarget(targets: string[]): Promise<Response> {
+    if (type == Type.RELAY) return fetchRelay(targets);
+    if (type == Type.BRIDGE) return fetchBridge(targets);
+    if (type == Type.ALL) return fetchAll(targets);
+    return Promise.reject();
   }
 
-  async function getData(targets: string[]): Promise<DataRow[]> {
-    return Promise.all<DataRow[]>(
-      targets.map(target => fetch(target))
-    ).then(results => {
-      return results.flatMap(data => data);
-    });
+  async function getData(targets: string[]): Promise<Response> {
+    return fetchTarget(targets);
   }
 
   function deleteTarget(idx: number) {
@@ -196,7 +196,7 @@ export default function Index() {
             {
               targets.map((target, i) => (
                 <Col key={i} md={24} lg={12} xxl={8} style={{width: '100%'}}>
-                  <TargetPlot stats={target.data} targetName={target.name} onDelete={() => deleteTarget(i)}></TargetPlot>
+                  <TargetPlot stats={target.data} anomalies={target.anomalies} targetName={target.name} onDelete={() => deleteTarget(i)}></TargetPlot>
                 </Col>
               ))
             }
@@ -210,15 +210,13 @@ export default function Index() {
             }}
           />
         <Modal title="Новый объект интереса" 
-          open={isModalOpen} destroyOnClose
-          onOk={onModalOk} onCancel={onModalCancel} 
+          open={isModalOpen} destroyOnClose onCancel={onModalCancel} 
           okButtonProps={{ autoFocus: true, htmlType: 'submit' }}
           modalRender={(dom) => (
             <Form
               layout="vertical"
               form={form}
               name="newTargetForm"
-              initialValues={{ modifier: 'public' }}
               clearOnDestroy
               onFinish={onFinish}
             >
--- components/ui/target-plot.tsx
+++ components/ui/target-plot.tsx
@@ -2,15 +2,16 @@ import { Area, Heatmap } from "@ant-design/plots";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { Button, Card, ConfigProvider, Dropdown, Flex, Modal, Segmented, theme } from "antd";
 import { AreaChartOutlined, DeleteOutlined, HeatMapOutlined } from "@ant-design/icons"
-import React, { useContext, useState } from "react";
+import React, { useCallback, useContext, useState } from "react";
 import moment from "moment";
+import { Anomaly } from "@/app";
 
 enum Type {
     LINE,
     HEAT
 }
 
-export default function TargetPlot({stats, targetName, onDelete} : any) {
+export default function TargetPlot({stats, anomalies, targetName, onDelete} : any) {
     const t = useContext(ConfigProvider.ConfigContext).theme;
     const [isModalOpen, setIsModalOpen] = useState<boolean>(false)
     const [type, setType] = useState<Type>(Type.LINE);
@@ -19,17 +20,39 @@ export default function TargetPlot({stats, targetName, onDelete} : any) {
         data: stats ?? [],
         padding: 'auto',
         xField: 'date',
-        yField: 'value',
+        yField: 'users',
         loading: stats == undefined,
         slider: {
             x: {
                 values: [0.8, 1],
             },
         },
-        colorField: 'target',
+        colorField: 'country',
         animate: { enter: { type: 'growInX' } },
         stack: true,
         height: 350,
+        annotations: [
+            {
+              type: "rangeX",
+              data: anomalies?.filter((anomaly: Anomaly) => anomaly.interval.start != anomaly.interval.end)
+              .map((anomaly: Anomaly) => ({
+                date: [anomaly.interval.start, anomaly.interval.end],
+                event: "hz"
+              })),
+              xField: 'date',
+              yField: 'event',
+              scale: { color: { independent: true, color: ["#FAAD14"] } },
+              style: { fillOpacity: 0.35, fill: '#FAAD14' },
+            },
+            {
+                type: "lineX",
+                data: anomalies?.filter((anomaly: Anomaly) => anomaly.interval.start == anomaly.interval.end)
+                    .map((anomaly: Anomaly) => ({date: anomaly.interval.start})),
+                xField: 'date',
+                style: { stroke: "#FAAD14", strokeOpacity: 0.25, lineWidth: 2 }
+            }
+          ],
+        sort: {fields: ['date']},
         theme: t?.algorithm == theme.darkAlgorithm ? 'classicDark' : 'classic'
     };
 
@@ -37,26 +60,30 @@ export default function TargetPlot({stats, targetName, onDelete} : any) {
         return moment(dateString).startOf("week").format("YYYY-MM-DD")
     };
 
-    function groupData(data: any[]) {
+    const groupData = useCallback(
+      () => {
         let dateCount = new Map<string, number>();
 
-        data?.forEach(row => {
-            dateCount.set(row.date, (dateCount.get(row.date) ?? 0) + row.value)
+        stats?.forEach((row: { date: string; users: number; }) => {
+            dateCount.set(row.date, (dateCount.get(row.date) ?? 0) + row.users)
         })
 
-        return [...dateCount.entries().map(entry => ({'date': entry[0], 'value': entry[1]}))];
-    }
+        return [...dateCount.entries().map(entry => ({'date': entry[0], 'users': entry[1]}))];
+      },
+      [stats],
+    )
+
 
     const heatConfig = {
-        data: groupData(stats),
+        data: groupData(),
         xField: (value: any) => getISOWeekStart(value.date),
         yField: (value: any) => new Date(value.date).toLocaleString("ru", { weekday: "short" }),
-        colorField: 'value',
+        colorField: 'users',
         legend: {},
         mark: 'cell',
         tooltip: {
-          title: 'value',
-          field: 'value',
+          title: 'users',
+          field: 'users',
           name: 'Кол-во пользователей'
         },
         animate: null,
@@ -65,7 +92,7 @@ export default function TargetPlot({stats, targetName, onDelete} : any) {
                 values: [0.8, 1],
             },
         },
-        sizeField: "target",
+        sizeField: "country",
         theme: t?.algorithm == theme.darkAlgorithm ? 'classicDark' : 'classic'
       };
 
@@ -85,7 +112,7 @@ export default function TargetPlot({stats, targetName, onDelete} : any) {
                 <Button type="text"><FontAwesomeIcon icon={"ellipsis-vertical"}/></Button>
             </Dropdown>
             } onDoubleClick={() => setIsModalOpen(true)} style={{ height: "100%", width: "100%", display: "flex", flexDirection: "column" }}>
-            <Area {...config} />
+            <Area {...config}/>
 
             <Modal title={targetName} open={isModalOpen} width="100%" onClose={() => setIsModalOpen(false)} onCancel={() => setIsModalOpen(false)} destroyOnClose>
                 <Flex justify="end">
@@ -97,7 +124,7 @@ export default function TargetPlot({stats, targetName, onDelete} : any) {
                         ]}
                     />
                 </Flex>
-                {type == Type.LINE && <Area {...config} />}
+                {type == Type.LINE && <Area {...config}/>}
                 {type == Type.HEAT && <Heatmap {...heatConfig} />}
             </Modal>
         </Card>
